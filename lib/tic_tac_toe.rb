d74107ed8c6c92f58d99ea9feff87f56638bd561
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 17:50:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,8 +73,8 @@ class TicTacToe
     end
   end
 
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board, index)
   end
 
 # def turn(board)
