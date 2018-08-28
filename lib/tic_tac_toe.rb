01a26bf6284018bbc8933b9661a22053fe409be4
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 17:51:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,18 +77,6 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(@board, index)
   end
 
-# def turn(board)
-#   puts "Please enter 1-9:"
-#   input = gets.strip
-#   index = input_to_index(input)
-#   if valid_move?(board, index)
-#     move(board, index, current_player(board))
-#     display_board(board)
-#   else
-#     puts "Invalid selection, try again"
-#     turn(board)
-#   end
-# end
 
 # def turn_count(board)
 #   counter = 0
